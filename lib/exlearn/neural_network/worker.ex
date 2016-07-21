1c9928789e0c11bcbecf1bc38a6558943865157d
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-07-21 10:39:04 +0300

    Implements state management for worker

--- lib/exlearn/neural_network/worker.ex
+++ lib/exlearn/neural_network/worker.ex
@@ -1,2 +1,51 @@
 defmodule ExLearn.NeuralNetwork.Worker do
+  @spec set_batch([{}], pid) :: atom
+  def set_batch(new_batch, worker) do
+    send worker, {:set, :batch, new_batch, self()}
+
+    receive do
+      message -> message
+    end
+  end
+
+  @spec set_configuration([{}], pid) :: atom
+  def set_configuration(new_configuration, worker) do
+    send worker, {:set, :configuration, new_configuration, self()}
+
+    receive do
+      message -> message
+    end
+  end
+
+  @spec set_network_state([{}], pid) :: atom
+  def set_network_state(new_network_state, worker) do
+    send worker, {:set, :network_state, new_network_state, self()}
+
+    receive do
+      message -> message
+    end
+  end
+
+  @spec start([{}], map, map) :: pid
+  def start(batch, configuration, network_state) do
+    spawn fn -> worker_loop(batch, configuration, network_state) end
+  end
+
+  @spec worker_loop([{}], map, map) :: no_return
+  defp worker_loop(batch, configuration, network_state) do
+    receive do
+      {:get, caller} ->
+        send caller, :ok
+        worker_loop(batch, configuration, network_state)
+      {:set, :batch, new_batch, caller} ->
+        send caller, :ok
+        worker_loop(new_batch, configuration, network_state)
+      {:set, :configuration, new_configuration, caller} ->
+        send caller, :ok
+        worker_loop(batch, new_configuration, network_state)
+      {:set, :network_state, new_network_state, caller} ->
+        send caller, :ok
+        worker_loop(batch, configuration, new_network_state)
+    end
+  end
 end
--- test/exlearn/neural_network/worker_test.exs
+++ test/exlearn/neural_network/worker_test.exs
@@ -2,4 +2,62 @@ defmodule WorkerTest do
   use ExUnit.Case, async: true
 
   alias ExLearn.NeuralNetwork.Worker
+
+  setup do
+    batch         = [{1, 1}, {2, 2}]
+    configuration = %{configuration: :configuration}
+    network_state = %{network_state: :network_state}
+
+    worker = Worker.start(batch, configuration, network_state)
+
+    {:ok, setup: %{
+      batch:         batch,
+      configuration: configuration,
+      network_state: network_state,
+      worker:        worker
+    }}
+  end
+
+  test "#set_batch receives success", %{setup: setup} do
+    %{worker: worker} = setup
+
+    new_batch = [{3, 3}, {4, 4}]
+
+    expected = :ok
+    result   = Worker.set_batch(new_batch, worker)
+
+    assert expected == result
+    assert Process.alive?(worker)
+  end
+
+  test "#set_configuration receives success", %{setup: setup} do
+    %{worker: worker} = setup
+
+    new_configuration = %{new_configuration: :new_configuration}
+
+    expected = :ok
+    result   = Worker.set_configuration(new_configuration, worker)
+
+    assert expected == result
+    assert Process.alive?(worker)
+  end
+
+  test "#set_network_state receives success", %{setup: setup} do
+    %{worker: worker} = setup
+
+    new_network_state = %{new_network_state: :new_network_state}
+
+    expected = :ok
+    result   = Worker.set_network_state(new_network_state, worker)
+
+    assert expected == result
+    assert Process.alive?(worker)
+  end
+
+  test "#start returns a running process", %{setup: setup} do
+    %{worker: worker} = setup
+
+    assert worker |> is_pid
+    assert Process.alive?(worker)
+  end
 end
