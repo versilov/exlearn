e0c569b5851ccbf0d2492191932c7eec2b0f083d
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-09-08 13:57:09 +0300

    More activity functions

--- native/lib/activity.c
+++ native/lib/activity.c
@@ -10,16 +10,16 @@ typedef void (*ActivityFunction)(float *);
 static void arctan_function(float *matrix) {
   int length = matrix[0] * matrix[1] + 2;
 
-  for(int index = 2; index < length; index += 1) {
+  for (int index = 2; index < length; index += 1) {
     matrix[index] = atan(matrix[index]);
   }
 }
 
 static void arctan_derivative(float *matrix) {
-  int length = matrix[0] * matrix[1] + 2;
-  int element;
+  int   length = matrix[0] * matrix[1] + 2;
+  float element;
 
-  for(int index = 2; index < length; index += 1) {
+  for (int index = 2; index < length; index += 1) {
     element = matrix[index];
 
     matrix[index] = 1.0 / (element * element + 1.0);
@@ -27,10 +27,10 @@ static void arctan_derivative(float *matrix) {
 }
 
 static void bent_identity_function(float *matrix) {
-  int length = matrix[0] * matrix[1] + 2;
-  int element;
+  int   length = matrix[0] * matrix[1] + 2;
+  float element;
 
-  for(int index = 2; index < length; index += 1) {
+  for (int index = 2; index < length; index += 1) {
     element = matrix[index];
 
     matrix[index] = (sqrt(element * element + 1.0) - 1.0) / 2.0 + element;
@@ -38,10 +38,10 @@ static void bent_identity_function(float *matrix) {
 }
 
 static void bent_identity_derivative(float *matrix) {
-  int length = matrix[0] * matrix[1] + 2;
-  int element;
+  int   length = matrix[0] * matrix[1] + 2;
+  float element;
 
-  for(int index = 2; index < length; index += 1) {
+  for (int index = 2; index < length; index += 1) {
     element = matrix[index];
 
     matrix[index] = element / (2.0 * sqrt(element * element + 1.0)) + 1.0;
@@ -49,10 +49,10 @@ static void bent_identity_derivative(float *matrix) {
 }
 
 static void gaussian_function(float *matrix) {
-  int length = matrix[0] * matrix[1] + 2;
-  int element;
+  int   length = matrix[0] * matrix[1] + 2;
+  float element;
 
-  for(int index = 2; index < length; index += 1) {
+  for (int index = 2; index < length; index += 1) {
     element = matrix[index];
 
     matrix[index] = exp(-element * element);
@@ -60,10 +60,10 @@ static void gaussian_function(float *matrix) {
 }
 
 static void gaussian_derivative(float *matrix) {
-  int length = matrix[0] * matrix[1] + 2;
-  int element;
+  int   length = matrix[0] * matrix[1] + 2;
+  float element;
 
-  for(int index = 2; index < length; index += 1) {
+  for (int index = 2; index < length; index += 1) {
     element = matrix[index];
 
     matrix[index] = -2.0 * element * exp(-element * element);
@@ -77,8 +77,88 @@ static void identity_function(float *matrix) {
 static void identity_derivative(float *matrix) {
   int length = matrix[0] * matrix[1] + 2;
 
-  for(int index = 2; index < length; index += 1) {
+  for (int index = 2; index < length; index += 1) {
     matrix[index] = 1.0;
+  }
+}
+
+static void logistic_function(float *matrix) {
+  int length = matrix[0] * matrix[1] + 2;
+
+  for (int index = 2; index < length; index += 1) {
+    if      (matrix[index] >  709) matrix[index] = 1.0;
+    else if (matrix[index] < -709) matrix[index] = 0.0;
+    else                           matrix[index] = 1.0 / (1.0 + exp(-matrix[index]));
+  }
+}
+
+static void logistic_derivative(float *matrix) {
+  int   length = matrix[0] * matrix[1] + 2;
+  float element;
+
+  for (int index = 2; index < length; index += 1) {
+    if      (matrix[index] >  709) element = 1.0;
+    else if (matrix[index] < -709) element = 0.0;
+    else                           element = 1.0 / (1.0 + exp(matrix[index]));
+
+    matrix[index] = element * (1.0 - element);
+  }
+}
+
+static void relu_function(float *matrix) {
+  int length = matrix[0] * matrix[1] + 2;
+
+  for (int index = 2; index < length; index += 1) {
+    if (matrix[index] <  0) matrix[index] = 0.0;
+  }
+}
+
+static void relu_derivative(float *matrix) {
+  int length = matrix[0] * matrix[1] + 2;
+
+  for (int index = 2; index < length; index += 1) {
+    if (matrix[index] <  0) matrix[index] = 0.0;
+    else                    matrix[index] = 1.0;
+  }
+}
+
+static void sinc_function(float *matrix) {
+  int   length = matrix[0] * matrix[1] + 2;
+  float element;
+
+  for (int index = 2; index < length; index += 1) {
+    element = matrix[index];
+
+    if (element == 0.0) matrix[index] = 1.0;
+    else                matrix[index] = sin(element) / element;
+  }
+}
+
+static void sinc_derivative(float *matrix) {
+  int   length = matrix[0] * matrix[1] + 2;
+  float element;
+
+  for (int index = 2; index < length; index += 1) {
+    element = matrix[index];
+
+    if (element != 0.0)
+      matrix[index] = cos(element) / element - sin(element) / (element * element);
+  }
+}
+
+static void sinusoid_function(float *matrix) {
+  int length = matrix[0] * matrix[1] + 2;
+
+  for (int index = 2; index < length; index += 1) {
+    matrix[index] = sin(matrix[index]);
+  }
+}
+
+static void sinusoid_derivative(float *matrix) {
+  int length = matrix[0] * matrix[1] + 2;
+
+  for (int index = 2; index < length; index += 1) {
+    matrix[index] = cos(matrix[index]);
   }
 }
 
@@ -88,15 +168,15 @@ static void identity_derivative(float *matrix) {
 
 static ActivityFunction
 activity_determine_function(int function_id, float alpha) {
-  switch(function_id) {
+  switch (function_id) {
     case  0: return arctan_function;
     case  1: return bent_identity_function;
     case  2: return gaussian_function;
     case  3: return identity_function;
-    // case  4: return logistic_function;
-    // case  5: return relu_function;
-    // case  6: return sinc_function;
-    // case  7: return sinusoid_function;
+    case  4: return logistic_function;
+    case  5: return relu_function;
+    case  6: return sinc_function;
+    case  7: return sinusoid_function;
     // case  8: return softmax_function;
     // case  9: return softplus_function;
     // case 10: return softsign_function;
@@ -109,15 +189,15 @@ activity_determine_function(int function_id, float alpha) {
 
 static ActivityFunction
 activity_determine_derivative(int function_id, float alpha) {
-  switch(function_id) {
+  switch (function_id) {
     case  0: return arctan_derivative;
     case  1: return bent_identity_derivative;
     case  2: return gaussian_derivative;
     case  3: return identity_derivative;
-    // case  4: return logistic_derivative;
-    // case  5: return relu_derivative;
-    // case  6: return sinc_derivative;
-    // case  7: return sinusoid_derivative;
+    case  4: return logistic_derivative;
+    case  5: return relu_derivative;
+    case  6: return sinc_derivative;
+    case  7: return sinusoid_derivative;
     // case  8: return softmax_derivative;
     // case  9: return softplus_derivative;
     // case 10: return softsign_derivative;
--- test/c/activity_test.c
+++ test/c/activity_test.c
@@ -101,6 +101,93 @@ static void test_the_identity_pair() {
   }
 }
 
+static void test_the_logistic_pair() {
+  int function_id = 4;
+
+  ActivityFunction function   = activity_determine_function(function_id, 0);
+  ActivityFunction derivative = activity_determine_derivative(function_id, 0);
+
+  float function_input[]   = {1, 3, 10, 710, -710};
+  float derivative_input[] = {1, 1, 10};
+
+  float expected_from_function[]   = {1, 3, 0.9999546021312976, 1.0, 0.0};
+  float expected_from_derivative[] = {1, 1, 4.5395805e-5};
+
+  function(function_input);
+  derivative(derivative_input);
+
+  for (int index = 0; index < 5; index += 1) {
+    assert(function_input[index]   == expected_from_function[index]  );
+  }
+
+  for (int index = 0; index < 3; index += 1) {
+    assert(derivative_input[index] == expected_from_derivative[index]);
+  }
+}
+
+static void test_the_relu_pair() {
+  int function_id = 5;
+
+  ActivityFunction function   = activity_determine_function(function_id, 0);
+  ActivityFunction derivative = activity_determine_derivative(function_id, 0);
+
+  float function_input[]   = {1, 3, -2, 0, 2};
+  float derivative_input[] = {1, 3, -2, 0, 2};
+
+  float expected_from_function[]   = {1, 3, 0, 0, 2};
+  float expected_from_derivative[] = {1, 3, 0, 1, 1};
+
+  function(function_input);
+  derivative(derivative_input);
+
+  for (int index = 0; index < 5; index += 1) {
+    assert(function_input[index]   == expected_from_function[index]  );
+    assert(derivative_input[index] == expected_from_derivative[index]);
+  }
+}
+
+static void test_the_sinc_pair() {
+  int function_id = 6;
+
+  ActivityFunction function   = activity_determine_function(function_id, 0);
+  ActivityFunction derivative = activity_determine_derivative(function_id, 0);
+
+  float function_input[]   = {1, 2, 0, 1};
+  float derivative_input[] = {1, 2, 0, 1};
+
+  float expected_from_function[]   = {1, 2, 1,  0.8414709848078965 };
+  float expected_from_derivative[] = {1, 2, 0, -0.30116867893975674};
+
+  function(function_input);
+  derivative(derivative_input);
+
+  for (int index = 0; index < 4; index += 1) {
+    assert(function_input[index]   == expected_from_function[index]  );
+    assert(derivative_input[index] == expected_from_derivative[index]);
+  }
+}
+
+static void test_the_sinusoid_pair() {
+  int function_id = 7;
+
+  ActivityFunction function   = activity_determine_function(function_id, 0);
+  ActivityFunction derivative = activity_determine_derivative(function_id, 0);
+
+  float function_input[]   = {1, 1, 0};
+  float derivative_input[] = {1, 1, 0};
+
+  float expected_from_function[]   = {1, 1, 0};
+  float expected_from_derivative[] = {1, 1, 1};
+
+  function(function_input);
+  derivative(derivative_input);
+
+  for (int index = 0; index < 3; index += 1) {
+    assert(function_input[index]   == expected_from_function[index]  );
+    assert(derivative_input[index] == expected_from_derivative[index]);
+  }
+}
+
 //-----------------------------------------------------------------------------
 // Run Tests
 //-----------------------------------------------------------------------------
@@ -111,6 +198,10 @@ int main() {
   test_the_bent_identity_pair();
   test_the_gaussian_pair();
   test_the_identity_pair();
+  test_the_logistic_pair();
+  test_the_relu_pair();
+  test_the_sinc_pair();
+  test_the_sinusoid_pair();
 
   return 0;
 }
