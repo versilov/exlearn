6f84093e4feaea9aef866605515fca20ede9003b
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-09-11 10:23:50 +0300

    Implements forward_for_activity

--- native/lib/neural_network/forwarder.c
+++ native/lib/neural_network/forwarder.c
@@ -12,9 +12,56 @@ forward_for_activity(
   NetworkState     *state,
   Matrix            sample
 ) {
-  int layers = structure->layers;
-
+  int       layers   = structure->layers;
   Activity *activity = new_activity(layers);
+  Matrix    input, output, mask, biases, weights;
+
+  input = new_matrix(sample[0], sample[1]);
+  clone_matrix(input, sample);
+
+  if (structure->dropout[0]) {
+    mask = create_dropout_mask(input[0], input[1], structure->dropout[0]);
+
+    matrix_multiply(input, mask, input);
+
+    activity->mask[0] = mask;
+  }
+  else {
+    activity->mask[0] = NULL;
+  }
+
+  output = input;
+
+  activity->input[0]  = NULL;
+  activity->output[0] = output;
+
+  for (int layer = 1; layer < layers; layer += 1) {
+    biases  = state->biases[layer];
+    weights = state->weights[layer];
+
+    input = new_matrix(output[0], weights[1]);
+    matrix_dot_and_add(output, weights, biases, input);
+
+    activity->input[layer] = input;
+
+    if (structure->dropout[layer]) {
+      mask = create_dropout_mask(input[0], input[1], structure->dropout[layer]);
+    }
+    else {
+      mask = NULL;
+    }
+
+    activity->mask[layer] = mask;
+
+    output = new_matrix(input[0], input[1]);
+    clone_matrix(output, input);
+
+    call_activity_closure(structure->function[layer], output);
+
+    if (mask != NULL) matrix_multiply(output, mask, output);
+
+    activity->output[layer] = output;
+  }
 
   return activity;
 }
