2a26412fa7942114b7bca143b9cc1c119889a148
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-09-12 17:24:25 +0300

    Fixture for backpropagation with dropout

--- test/c/fixtures/correction_fixtures.c
+++ test/c/fixtures/correction_fixtures.c
@@ -42,4 +42,43 @@ correction_expected_basic() {
   return correction;
 }
 
+static Correction *
+correction_expected_with_dropout() {
+  Correction *correction = new_correction(4);
+
+  // First Hidden Layer Correction
+  float layer_1_biases_correction[5]   = {1, 3, 336, 672, 0};
+  float layer_1_weights_correction[11] = {3, 3,
+    336, 672, 0, 672, 1344, 0, 1008, 2016, 0
+  };
+
+  correction->biases[1]  = new_matrix(1, 3);
+  correction->weights[1] = new_matrix(3, 3);
+
+  clone_matrix(correction->biases[1],  layer_1_biases_correction );
+  clone_matrix(correction->weights[1], layer_1_weights_correction);
+
+  // Second Hidden Layer Correction
+  float layer_2_biases_correction[4]  = {1, 2, 0, 84};
+  float layer_2_weights_correction[8] = {3, 2, 0, 2604, 0, 3192, 0, 3780};
+
+  correction->biases[2]  = new_matrix(1, 2);
+  correction->weights[2] = new_matrix(3, 2);
+
+  clone_matrix(correction->biases[2],  layer_2_biases_correction );
+  clone_matrix(correction->weights[2], layer_2_weights_correction);
+
+  // Output Layer Correction
+  float layer_3_biases_correction[4]  = {1, 2, 30, -12};
+  float layer_3_weights_correction[6] = {2, 2, 11130, -4452, 14580, -5832};
+
+  correction->biases[3]  = new_matrix(1, 2);
+  correction->weights[3] = new_matrix(2, 2);
+
+  clone_matrix(correction->biases[3],  layer_3_biases_correction );
+  clone_matrix(correction->weights[3], layer_3_weights_correction);
+
+  return correction;
+}
+
 #endif
--- test/c/neural_network/propagator_test.c
+++ test/c/neural_network/propagator_test.c
@@ -34,12 +34,28 @@ static void test_back_propagate() {
 }
 
 static void test_back_propagate_with_dropout() {
-  NetworkStructure *structure = network_structure_with_dropout();
-  NetworkState     *state     = network_state_basic();
-  Matrix            expected  = data_expected_basic();
-  Activity         *activity  = activity_expected_with_dropout();
+  NetworkStructure *structure           = network_structure_with_dropout();
+  NetworkState     *state               = network_state_basic();
+  Matrix            expected_output     = data_expected_basic();
+  Activity         *activity            = activity_expected_with_dropout();
+  Correction       *expected_correction = correction_expected_with_dropout();
 
-  Correction *correction = back_propagate(structure, state, activity, expected);
+  Correction *result = back_propagate(structure, state, activity, expected_output);
 
-  assert(correction != NULL);
+  assert(result->layers == expected_correction->layers);
+
+  assert(result->biases[0]  == NULL);
+  assert(result->weights[0] == NULL);
+
+  for (int layer = 1; layer < result->layers; layer += 1) {
+    assert(matrix_equal(result->biases[layer],  expected_correction->biases[layer]));
+    assert(matrix_equal(result->weights[layer], expected_correction->weights[layer]));
+  }
+
+  free_correction(result);
+  free_correction(expected_correction);
+  free_activity(activity);
+  free_matrix(expected_output);
+  free_network_state(state);
+  free_network_structure(structure);
 }
