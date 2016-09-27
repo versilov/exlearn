b6bb35547a3480c81865bf06ba3efb2e5736abe4
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-09-27 11:26:50 +0300

    Finishes documenting file format

--- native/lib/worker/worker_data.c
+++ native/lib/worker/worker_data.c
@@ -60,7 +60,42 @@ new_worker_data() {
 //   jump          :: [0|1]
 //                     0: Do not discard samples.
 //                     1: Discard first (maximum_step - step) samples.
-//   data          :: The binary data.
+//     Example:
+//       Starting with the following hypothetical samples:
+//       [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+//
+//       Split them in 3 files with a maximum step of 5:
+//       [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9]
+//       [ 6,  7,  8,  9, 10, 11, 12, 13, 14, 15]
+//       [12, 13, 14, 15, 16, 17, 18, 19, 20]
+//
+//       To get sequesnces of 3 samples we ignore the first (5-3) = 2 elements
+//       from the second and third file.
+//
+//       First file remains intact:
+//       [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//       And has the following sequences:
+//       [0, 1, 2], [1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7],
+//       [6, 7, 8], [7, 8 9]
+//
+//       Second file loses 6 and 7:
+//       [8, 9, 10, 11, 12, 13, 14, 15]
+//       And has the following sequesnces:
+//       [ 8,  9, 10], [9, 10, 11], [10, 11, 12], [11, 12, 13], [12, 13, 14],
+//       [13, 14, 15]
+//
+//       Third file loses 12 and 13:
+//       [14, 15, 16, 17, 18, 19, 20]
+//       And has the following sequesnces:
+//       [14, 15, 16], [15, 16, 17], [16, 17, 18], [17, 18, 19], [18, 19, 20]
+//
+//   data :: A list of samples. A sample is composed of two binaries.
+//     For Training:
+//       The first binary represents the input matrix.
+//       The second binary represents the expected output matrix.
+//     For Prediction:
+//       The first binary represents an identifier for the input matrix.
+//       The second binary represents the input matrix.
 static void
 read_worker_data(const char *path, WorkerData *data) {
   float  float_buffer;
