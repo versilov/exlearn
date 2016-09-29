7bb4d87b91844e134b8efa1b23ed212ca69e1d5f
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-09-29 15:33:00 +0300

    Adds back worker_nifs.c

---
+++ native/nifs/worker_nifs.c
@@ -0,0 +1,16 @@
+#include "erl_nif.h"
+
+static ERL_NIF_TERM
+create_worker_data(ErlNifEnv *env, int argc, const ERL_NIF_TERM *argv) {
+  (void)(env);
+  (void)(argc);
+  (void)(argv);
+
+  return 0;
+}
+
+static ErlNifFunc nif_functions[] = {
+  {"create_worker_data", 1, create_worker_data, 0}
+};
+
+ERL_NIF_INIT(Elixir.ExLearn.NeuralNetwork.Worker, nif_functions, NULL, NULL, NULL, NULL)
