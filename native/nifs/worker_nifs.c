d42e512fbe5283487b06d1cbc7cc5c8530460d09
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-09-30 14:29:21 +0300

    Extracts paths from erlang list

--- native/nifs/worker_nifs.c
+++ native/nifs/worker_nifs.c
@@ -1,9 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "erl_nif.h"
+
+#include "../lib/worker/bundle_paths.c"
 
 static ERL_NIF_TERM
 create_worker_data(ErlNifEnv *env, int argc, const ERL_NIF_TERM *argv) {
-  (void)(env);
+  BundlePaths  *bundle_paths;
+  ErlNifBinary  path;
+  ERL_NIF_TERM  list, head, tail;
+  unsigned int  index, length;
+  char         *new_path;
+
   (void)(argc);
+
+  list = argv[0];
+  if (!enif_is_list(env, list)) return enif_make_badarg(env);
+
+  enif_get_list_length(env, list, &length);
+  bundle_paths = bundle_paths_new(length);
+
+  index = 0;
+  tail  = list;
+  while (enif_get_list_cell(env, tail, &head, &tail)) {
+    if (!enif_inspect_binary(env, head, &path)) return enif_make_badarg(env);
+
+    new_path = malloc(sizeof(char) * path.size);
+    memcpy(new_path, path.data, path.size);
+
+    bundle_paths->path[index] = new_path;
+
+    index += 1;
+  }
+
+  bundle_paths_free(&bundle_paths);
 
   return argv[0];
 }
--- test/elixir/exlearn/neural_network/worker/nifs_test.exs
+++ test/elixir/exlearn/neural_network/worker/nifs_test.exs
@@ -4,6 +4,6 @@ defmodule ExLearn.NeuralNetwork.Worker.NifsTest do
   alias ExLearn.NeuralNetwork.Worker
 
   test "#create_worker_data can be called" do
-    assert Worker.create_worker_data([]) == []
+    assert Worker.create_worker_data(["/path/to/file1", "/path/to/file2"]) != []
   end
 end
