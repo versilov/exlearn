5e0d405a35cf0358f65b91d087ed12a74db6ec2b
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-11-21 13:54:10 +0200

    Upgrades elixir to 1.3.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExLearn.Mixfile do
     [
       app:               :ExLearn,
       version:           "0.1.0",
-      elixir:            "1.3.3",
+      elixir:            "1.3.4",
       build_embedded:    Mix.env == :prod,
       start_permanent:   Mix.env == :prod,
       deps:              deps,
