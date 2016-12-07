29f17f7f58d9d5bac5fb24432bf8e1409c09a59a
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-12-07 15:41:35 +0200

    Creating resource object for WorkerResource

--- native/nifs/worker_nifs.c
+++ native/nifs/worker_nifs.c
@@ -4,31 +4,20 @@
 
 #include "erl_nif.h"
 
-#include "../include/worker/batch_data.h"
-#include "../include/worker/bundle_paths.h"
-#include "../include/worker/worker_data.h"
+#include "../include/worker/worker_resource.h"
 
 //------------------------------------------------------------------------------
-// Resource definition
+// Destructor
 //------------------------------------------------------------------------------
 
-ErlNifResourceType *BATCH_DATA;
-ErlNifResourceType *WORKER_DATA;
+ErlNifResourceType *WORKER_RESOURCE;
 
-static void batch_data_destructor(ErlNifEnv *_env, void *resource_content) {
+static void worker_resource_destructor(ErlNifEnv *_env, void *resource_content) {
   (void)(_env);
 
-  BatchData *batch_data = (BatchData *)(resource_content);
+  WorkerResource *worker_resource = (WorkerResource *)(resource_content);
 
-  batch_data_free(&batch_data);
-}
-
-static void worker_data_destructor(ErlNifEnv *_env, void *resource_content) {
-  (void)(_env);
-
-  WorkerData *worker_data = (WorkerData *)(resource_content);
-
-  worker_data_free(&worker_data);
+  worker_resource_free(&worker_resource);
 }
 
 //------------------------------------------------------------------------------
@@ -36,69 +25,17 @@ static void worker_data_destructor(ErlNifEnv *_env, void *resource_content) {
 //------------------------------------------------------------------------------
 
 static ERL_NIF_TERM
-create_batch_data(ErlNifEnv *env, int32_t argc, const ERL_NIF_TERM *argv) {
-  BatchData    *batch_data;
-  WorkerData   *worker_data;
-  ERL_NIF_TERM  result;
-  int64_t       batch_length;
+create_worker_resource(ErlNifEnv *env, int32_t argc, const ERL_NIF_TERM *argv) {
+  ERL_NIF_TERM    result;
+  WorkerResource *worker_resource;
 
   (void)(argc);
+  (void)(argv);
 
-  if (!enif_get_resource(env, argv[0], WORKER_DATA, (void **)(&worker_data)))
-    return enif_make_badarg(env);
-  if (!enif_get_int64(env, argv[1], &batch_length))
-    return enif_make_badarg(env);
+  worker_resource = enif_alloc_resource(WORKER_RESOURCE, sizeof(WorkerResource));
 
-  batch_data = enif_alloc_resource(BATCH_DATA, sizeof(BatchData));
-  batch_data_initialize(batch_data, worker_data, batch_length);
-  shuffle_batch_data_indices(batch_data);
-
-  result = enif_make_resource(env, batch_data);
-  enif_release_resource(&batch_data);
-
-  return result;
-}
-
-static ERL_NIF_TERM
-create_worker_data(ErlNifEnv *env, int32_t argc, const ERL_NIF_TERM *argv) {
-  BundlePaths  *bundle_paths;
-  ERL_NIF_TERM  list, head, tail, result;
-  unsigned int  index, length, string_length;
-  char         *new_path;
-  WorkerData   *worker_data;
-
-  (void)(argc);
-
-  list = argv[0];
-  if (!enif_is_list(env, list)) return enif_make_badarg(env);
-
-  enif_get_list_length(env, list, &length);
-  bundle_paths = bundle_paths_new(length);
-
-  index = 0;
-  tail  = list;
-  while (enif_get_list_cell(env, tail, &head, &tail)) {
-    if (!enif_is_list(env, head)) return enif_make_badarg(env);
-
-    enif_get_list_length(env, head, &string_length);
-    new_path = malloc(sizeof(char) * string_length + 1);
-
-    if (!enif_get_string(env, head, new_path, string_length + 1, ERL_NIF_LATIN1))
-      return enif_make_badarg(env);
-
-    bundle_paths->paths[index] = new_path;
-
-    index += 1;
-  }
-
-  worker_data = enif_alloc_resource(WORKER_DATA, sizeof(WorkerData));
-  worker_data_initialize(worker_data, length);
-  worker_data_read(bundle_paths, worker_data);
-
-  result = enif_make_resource(env, worker_data);
-  enif_release_resource(&worker_data);
-
-  bundle_paths_free(&bundle_paths);
+  result = enif_make_resource(env, worker_resource);
+  enif_release_resource(&worker_resource);
 
   return result;
 }
@@ -108,8 +45,7 @@ create_worker_data(ErlNifEnv *env, int32_t argc, const ERL_NIF_TERM *argv) {
 //------------------------------------------------------------------------------
 
 static ErlNifFunc nif_functions[] = {
-  {"create_batch_data",  2, create_batch_data,  0},
-  {"create_worker_data", 1, create_worker_data, 0}
+  {"create_worker_resource", 0, create_worker_resource, 0}
 };
 
 static int32_t load(ErlNifEnv *env, void **_priv_data, ERL_NIF_TERM _load_info) {
@@ -118,12 +54,8 @@ static int32_t load(ErlNifEnv *env, void **_priv_data, ERL_NIF_TERM _load_info) 
 
   int32_t flags = ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER;
 
-  BATCH_DATA = enif_open_resource_type(
-    env, NULL, "BatchData", batch_data_destructor, flags, NULL
-  );
-
-  WORKER_DATA = enif_open_resource_type(
-    env, NULL, "WorkerData", worker_data_destructor, flags, NULL
+  WORKER_RESOURCE = enif_open_resource_type(
+    env, NULL, "WorkerResource", worker_resource_destructor, flags, NULL
   );
 
   return 0;
--- test/elixir/exlearn/neural_network/worker/nifs_test.exs
+++ test/elixir/exlearn/neural_network/worker/nifs_test.exs
@@ -1,6 +1,5 @@
 [
   "test/elixir/test_util.exs",
-  "test/elixir/fixtures/file_fixtures.exs",
 ]
 |> Enum.map(&Code.require_file/1)
 
@@ -9,42 +8,9 @@ defmodule ExLearn.NeuralNetwork.Worker.NifsTest do
 
   alias ExLearn.NeuralNetwork.Worker
 
-  alias ExLearn.TestUtil
-  alias ExLearn.FileFixtures
+  test "#create_worker_resource can be called" do
+    worker_resource = Worker.create_worker_resource()
 
-  test "#create_batch_data can be called" do
-    first_data  = FileFixtures.first_data_bundle
-    second_data = FileFixtures.second_data_bundle
-
-    first_path  = TestUtil.temp_file_path_as_list("exlearn-neural_network-worker-nifs_test_1")
-    second_path = TestUtil.temp_file_path_as_list("exlearn-neural_network-worker-nifs_test_2")
-
-    :ok  = File.write(first_path,  first_data )
-    :ok  = File.write(second_path, second_data)
-
-    worker_data = Worker.create_worker_data([first_path, second_path])
-    batch_data  = Worker.create_batch_data(worker_data, 2);
-
-    assert batch_data != []
-
-    :ok = File.rm(first_path )
-    :ok = File.rm(second_path)
-  end
-
-  test "#create_worker_data can be called" do
-    first_data  = FileFixtures.first_data_bundle
-    second_data = FileFixtures.second_data_bundle
-
-    first_path  = TestUtil.temp_file_path_as_list("exlearn-neural_network-worker-nifs_test_3")
-    second_path = TestUtil.temp_file_path_as_list("exlearn-neural_network-worker-nifs_test_4")
-
-    :ok  = File.write(first_path,  first_data )
-    :ok  = File.write(second_path, second_data)
-
-    result = Worker.create_worker_data([first_path, second_path])
-    assert result != []
-
-    :ok = File.rm(first_path )
-    :ok = File.rm(second_path)
+    assert worker_resource != nil
   end
 end
