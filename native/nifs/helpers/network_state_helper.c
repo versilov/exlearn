9e23126762277e2416570e6504357ecd40d3b2e9
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2017-02-22 09:47:36 +0200

    Extracts size from elixir map

--- native/nifs/helpers/network_state_helper.c
+++ native/nifs/helpers/network_state_helper.c
@@ -8,10 +8,14 @@ create_network_state_from_layers(
   (void)(worker_resource);
   (void)(network_definition);
 
-  ERL_NIF_TERM layers_atom, input_atom, hidden_atom, output_atom,
-               size_atom, activation_atom, dropout_atom, name_atom;
-  ERL_NIF_TERM layers_map, input_map, hidden_list, hidden_map, output_map;
-  uint32_t     hidden_length;
+  ERL_NIF_TERM  layers_atom, input_atom, hidden_atom, output_atom,
+                size_atom, activation_atom, dropout_atom, name_atom;
+  ERL_NIF_TERM  columns_int, rows_int;
+  ERL_NIF_TERM  hidden_list;
+  ERL_NIF_TERM  layers_map, input_map, hidden_map, output_map;
+  NetworkState *network_state;
+  int32_t       columns, rows;
+  uint32_t      hidden_length;
 
   layers_atom = enif_make_atom(env, "layers");
   enif_get_map_value(env, network_definition, layers_atom, &layers_map);
@@ -25,16 +29,35 @@ create_network_state_from_layers(
   enif_get_map_value(env, layers_map, output_atom, &output_map );
 
   enif_get_list_length(env, hidden_list, &hidden_length);
-  worker_resource->network_state = network_state_new(hidden_length + 2);
+
+  network_state = network_state_new(hidden_length + 2);
+  worker_resource->network_state = network_state;
 
   size_atom       = enif_make_atom(env, "size"      );
   activation_atom = enif_make_atom(env, "activation");
   dropout_atom    = enif_make_atom(env, "dropout"   );
   name_atom       = enif_make_atom(env, "name"      );
 
-  for (int index = 0; index < hidden_length; index += 1) {
+  enif_get_map_value(env, input_map, size_atom, &columns_int);
+  enif_get_int(env, columns_int, &columns);
+  network_state->rows[0]    = 1;
+  network_state->columns[0] = columns;
+  rows = columns;
+
+  for (uint32_t index = 0; index < hidden_length; index += 1) {
     enif_get_list_cell(env, hidden_list, &hidden_map, &hidden_list);
+
+    enif_get_map_value(env, hidden_map, size_atom, &columns_int);
+    enif_get_int(env, columns_int, &columns);
+    network_state->rows[index + 1]    = rows;
+    network_state->columns[index + 1] = columns;
+    rows = columns;
   }
+
+  enif_get_map_value(env, output_map, size_atom, &columns_int);
+  enif_get_int(env, columns_int, &columns);
+  network_state->rows[hidden_length + 1]    = rows;
+  network_state->columns[hidden_length + 1] = columns;
 }
 
 static void
