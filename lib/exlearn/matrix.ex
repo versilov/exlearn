f2a40fa5302d89d066930e7f156abcae98433de5
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2017-04-26 18:46:24 +0300

    First attempt at a formal proof and subtle bug fixed in consequence

--- lib/exlearn/matrix.ex
+++ lib/exlearn/matrix.ex
@@ -11,10 +11,13 @@ defmodule ExLearn.Matrix do
   The length of the top list is used to determine the number of rows.
   The length of the first list inside the top list is used to determine the
   number of columns.
+
   If the internal lists are not of the same length or their contents are not
   numbers then the function will crash.
+  The lists must all be proper lists, otherwise the function will crash.
 
-  Example:
+  # Example
+
   ```elixir
   list_of_lists = [[1, 2, 3], [4, 5, 6]]
 
@@ -23,10 +26,92 @@ defmodule ExLearn.Matrix do
   #   128, 64, 0, 0, 160, 64, 0, 0, 192, 64>>
   ```
 
-  Parameters:
-  - `list_of_lists`: A `list` containing `lists` of the same length with values
+  # Parameters
+  1. `list_of_lists`: A `list` containing `lists` of the same length with values
   being `integers` or `floats`.
 
+  # Return value
+
+  The function returns a `binary` with the following structure:
+
+  ```
+  <<
+    rows    :: unsigned-integer-little-32,
+    columns :: unsigned-integer-little-32
+    first   :: float-little-32,
+    ...
+  >>
+  ```
+
+  The `...` denotes potentially more elements following, equal to the number of
+  elements in all the sublists, all having the same data type as `first`.
+
+  # Formal proof of Correctness
+
+  ```
+  THEOREM: The function produces valid output given valid input, otherwise it
+  produces a crash.
+
+  <1>1. LET =X= be the argument to the function.
+  <2>1. CASE =X= is not a list.
+    The function crashes on the first pattern match.
+  <2>2. CASE =X= is an improper list.
+    The function is guaranteed to crash when the `length` function is called on =X=
+  due to the way `length` works.
+  <2>3. CASE =X= is a proper list.
+  <3>1. LET =Y= be the first element in =X=.
+  <3>2. CASE =Y= is not a list.
+    The function crashes on the first pattern match.
+  <3>3. CASE =Y= is an improper list.
+    The function is guaranteed to crash when the `length` function is called on =Y=
+  due to the way `length` works.
+  <3>4. CASE =Y= is a proper list.
+  <4>1. LET =R= be the number of rows, equal to the length of =X=, a non negative
+  integer.
+  <4>2. LET =C= be the number of columns, equal to the length of =Y=, a non
+  negative integer.
+  <4>3. LET =I= be the initial binary, to withch the elements in =X= will be
+  concatenated after being transformed in binary form. =I= conforms to the return
+  value specification.
+  <4>4. FOR EACH element =L= inside the list =X= traversed in order; there is at
+  least one element =L= inside =X= that is a non empty proper list.
+  <5>1. CASE =L= is not a list.
+    The function crashes on the pattern match that extracts the head of a list.
+  <5>2. CASE =L= is an improper list.
+  <6>1. LET =Z= be the last element of the improper list =L=.
+    When =Z= is not an empty list, the argument guard will pass it to the function
+  that does a patter match in order to extract the head and tail. This pattern
+  match will fail and crash the function.
+  <5>.3 CASE =L= is a proper list.
+  <6>.1 FOR EACH element =E= inside the list =L=.
+  <7>.1 CASE =E= is not a number.
+    The function crashes when tryin to convert the term =E= into a binary
+  representation of a float.
+  <7>.2 CASE =E= is a number.
+    The term =E= is used to create a binary representation of a float which is
+  concatenated to the end of an accumulator, initially containing an empty
+  binary. The new accumulator is passed on to the next iteration.
+  <7>.3 The resulting binary is a valid binary representation of all the
+  elements inside =E=.
+  <7>.4 The resulting binary is concatenated at the end of the binary from the
+  previous iteration, or with an empty binary if we are at the start of the first
+  iteration.
+  <6>.2 CASE There are less elements in =L= than the value of =C=.
+    The list of remaining elements will be empty while the number of still to
+  process elements will be greater than 0. The function will try a pattern match
+  to extract head and tail from an empty list and crash.
+  <6>.3 CASE There are more elements in =L= than the value of =C=.
+    The iterating function expects an empty list when the remaining number of
+  elements is 0. When the remaining list is empty, the number of elements drops
+  below 0 and no guard matches so the function crashes.
+  <5>.2 The resulting binary is a valid binary representation of all the elements
+  inside =L=.
+  <5>.3 The resulting binary is concatenated at the end of the binary from the
+  previous iteration, or with =I= if we are at the start of the first iteration.
+  <4>.5 The resulting binary is a valid binary representation of a matrix.
+  <4>.6 Q.E.D.
+    The resulting binary is returned to the caller.
+  ```
   """
 
   @spec new(list(list)) :: binary
@@ -47,6 +132,7 @@ defmodule ExLearn.Matrix do
   If the length of the top list does no match the `rows`, the number of elements
   in each sublist does not match the `columns`, or the elements are not all
   numbers the function will crash.
+  The lists must all be proper lists, otherwise the function will crash.
 
   Example:
   ```elixir
@@ -90,7 +176,7 @@ defmodule ExLearn.Matrix do
   end
 
   defp binary_from_list(0,       [],   accumulator), do: accumulator
-  defp binary_from_list(columns, list, accumulator)  do
+  defp binary_from_list(columns, list, accumulator) when columns > 0 do
     [head|tail] = list
 
     new_accumulator = accumulator <> <<head :: float-little-32>>
