f8e4f550bb02de3f981e8d499795268168e64fb6
Stas Versilov <stas.versilov@gmail.com>
2017-07-10 12:16:08 +0400

    Benchmark samples in module doc.

--- bench/matrix_bench.exs
+++ bench/matrix_bench.exs
@@ -13,7 +13,40 @@ end
 defmodule MatrixBench do
   @moduledoc """
   Benchfella module to compare matrix operations performance with a115/exmatrix
+
+  Below are results for MacBookPro, 8 cores, 16 GB memory.
+  ExLearn used only a fraction of CPU, because it does not use parallel computation.
+  So, actual performance advantage of ExLearn will be an order of magnitude greater.
+
+  ## ExMatrix
+
+  Finished in 29.04 seconds
+
+  benchmark name                iterations   average time
+  transpose a 100x100 matrix        5000   719.77 µs/op
+  transpose a 200x200 matrix         500   2828.41 µs/op
+  transpose a 400x400 matrix         100   14057.79 µs/op
+  50x50 matrix in parallel            50   29563.38 µs/op
+  100x100 matrix in parallel          10   195277.70 µs/op
+  200x200 matrix in parallel           1   1585938.00 µs/op
+  400x400 matrix in parallel           1   15745453.00 µs/op
+
+
+  ## ExLearn.Matrix
+
+  Finished in 19.57 seconds
+
+  benchmark name                iterations   average time
+  transpose a 100x100 matrix        100000   27.47 µs/op
+  50x50 matrices dot product         20000   95.70 µs/op
+  transpose a 200x200 matrix         10000   120.89 µs/op
+  transpose a 400x400 matrix          5000   418.17 µs/op
+  100x100 matrices dot product        5000   674.94 µs/op
+  200x200 matrices dot product         500   4998.85 µs/op
+  400x400 matrices dot product          50   38847.84 µs/op
+
   """
+
   use Benchfella
   import ExLearn.Matrix
   import MatrixBench.RandomMatrix
