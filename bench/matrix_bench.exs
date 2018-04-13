21a66644ca79b2f67ea1e345456b849412f1a362
Stas Versilov <stas.versilov@gmail.com>
2018-04-13 15:28:39 +0400

    Update bench results for MacOS native compile (without docker)

--- bench/matrix_bench.exs
+++ bench/matrix_bench.exs
@@ -34,16 +34,18 @@ defmodule MatrixBench do
 
   ## ExLearn.Matrix
   ```
-  Finished in 18.4 seconds
+  Finished in 19.3 seconds
 
+  ## MatrixBench
   benchmark name                iterations   average time
-  transpose a 100x100 matrix        100000   26.28 µs/op
-  50x50 matrices dot product         20000   89.86 µs/op
-  transpose a 200x200 matrix         10000   116.80 µs/op
-  transpose a 400x400 matrix          5000   405.55 µs/op
-  100x100 matrices dot product        5000   622.99 µs/op
-  200x200 matrices dot product         500   4652.35 µs/op
-  400x400 matrices dot product          50   36277.00 µs/op
+  50x50 matrices dot product        500000   6.89 µs/op
+  transpose a 100x100 matrix        100000   28.55 µs/op
+  100x100 matrices dot product       50000   37.50 µs/op
+  200x200 matrices dot product       10000   124.31 µs/op
+  transpose a 200x200 matrix         10000   126.82 µs/op
+  transpose a 400x400 matrix          5000   441.10 µs/op
+  400x400 matrices dot product        5000   673.23 µs/op
+
   ```
   """
 
